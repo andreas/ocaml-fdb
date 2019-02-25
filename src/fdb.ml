17093cb400cced4a7dfffc57de05c27b9f15ab67
Andreas Garnaes <andreas.garnaes@gmail.com>
2019-02-25 14:36:04 +0100

    Fix convenience functions on Database

--- src/fdb.ml
+++ src/fdb.ml
@@ -368,13 +368,13 @@ module Make (Io : IO) = struct
       return (transaction t) >>=? fun tx -> Transaction.commit_with_retry tx ~f
 
     let get ?snapshot t ~key =
-      with_tx t ~f:(fun tx -> Transaction.get ?snapshot t ~key)
+      with_tx t ~f:(fun tx -> Transaction.get ?snapshot tx ~key)
 
     let get_bigstring ?snapshot t ~key =
-      with_tx t ~f:(fun tx -> Transaction.get_bigstring ?snapshot t ~key)
+      with_tx t ~f:(fun tx -> Transaction.get_bigstring ?snapshot tx ~key)
 
     let get_key ?snapshot t ~key_selector =
-      with_tx t ~f:(fun tx -> Transaction.get_key ?snapshot t ~key_selector)
+      with_tx t ~f:(fun tx -> Transaction.get_key ?snapshot tx ~key_selector)
 
     let get_range ?limit ?target_bytes ?snapshot ?reverse ?mode t ~start ~stop =
       with_tx t ~f:(fun tx -> Transaction.get_range ?limit ?target_bytes ?snapshot ?reverse ?mode t ~start ~stop)
@@ -383,10 +383,10 @@ module Make (Io : IO) = struct
       with_tx t ~f:(fun tx -> Transaction.get_range_prefix ?limit ?target_bytes ?snapshot ?reverse ?mode t ~prefix)
 
     let set t ~key ~value =
-      with_tx t ~f:(fun tx -> return (Ok (Transaction.set t ~key ~value)))
+      with_tx t ~f:(fun tx -> return (Ok (Transaction.set tx ~key ~value)))
 
     let set_bigstring t ~key ~value =
-      with_tx t ~f:(fun tx -> return (Ok (Transaction.set_bigstring t ~key ~value)))
+      with_tx t ~f:(fun tx -> return (Ok (Transaction.set_bigstring tx ~key ~value)))
 
     let clear t ~key =
       with_tx t ~f:(fun tx -> return (Ok (Transaction.clear tx ~key)))
